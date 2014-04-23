0f766f6009ed620f40f22a75b733daff87bfa66d
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-04-23 13:15:59 -0700

    adding monsters.rb

--- monsters.rb
+++ monsters.rb
@@ -42,13 +42,12 @@ puts $monsters.count{ |m| m[:legs] == 2 }
 #puts $monsters.select{|m| m[:legs]== 2}.map{|m| m[:name]}
 
 puts "How many monsters are nocturnal?"
-puts # your answer here
+puts $monsters.count {|m| m[:nocturnal] == true }
 puts "What are the names of the monsters that are nocturnal?"
-puts # your answer here
+puts $monsters.select {|m| m[:nocturnal] == true }.map{|m| m [:name]}
 puts "How many legs do all our monsters have?"
-puts # your answer here
+puts $monsters.map {|m| m[:legs]}.inject(:+)
 puts "What are the 2 most common dangers of our monsters?"
-puts # your answer here
+puts $monsters.flat_map {|m| m[:dangers]}.group_by {|m| m}.values.sort_by {|x| -x.length}.flatten.uniq.first(2)
 puts "What are the 2 most common vulnerabilities of our monsters?"
-puts # your answer here
-
+puts $monsters.flat_map {|m| m[:vulnerabilities]}.group_by {|m| m}.values.sort_by {|x| -x.length}.flatten.uniq.first(2)
